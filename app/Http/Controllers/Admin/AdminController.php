66ead215609be748fc8c122900d44ba84b8b4c31
GANLAXMINE <ganlaxmineprivatelimited@gmail.com>
2024-07-10 21:27:13 +0530

    changes

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -25,33 +25,33 @@ class AdminController extends Controller
     {
         return view('admin.auth.login');
     }
-    public function userMatchCount()
-    {
-        // Get distinct pairs of matched users
-        $uniqueMatches = UserMatch::select('user_id', 'match_user')
-            ->distinct()
-            ->get();
+    // public function userMatchCount()
+    // {
+    //     // Get distinct pairs of matched users
+    //     $uniqueMatches = UserMatch::select('user_id', 'match_user')
+    //         ->distinct()
+    //         ->get();
 
-        // Initialize an empty array to store unique match IDs
-        $uniqueMatchIds = [];
+    //     // Initialize an empty array to store unique match IDs
+    //     $uniqueMatchIds = [];
 
-        // Loop through the distinct pairs and store the first match ID encountered
-        foreach ($uniqueMatches as $match) {
-            $matchId = UserMatch::where('user_id', $match->user_id)
-                ->where('match_user', $match->match_user)
-                ->orWhere('user_id', $match->match_user)
-                ->where('match_user', $match->user_id)
-                ->orderBy('created_at', 'asc') // Change this if you want to order by a different criteria
-                ->value('id');
+    //     // Loop through the distinct pairs and store the first match ID encountered
+    //     foreach ($uniqueMatches as $match) {
+    //         $matchId = UserMatch::where('user_id', $match->user_id)
+    //             ->where('match_user', $match->match_user)
+    //             ->orWhere('user_id', $match->match_user)
+    //             ->where('match_user', $match->user_id)
+    //             ->orderBy('created_at', 'asc') // Change this if you want to order by a different criteria
+    //             ->value('id');
 
-            if (!in_array($matchId, $uniqueMatchIds)) {
-                $uniqueMatchIds[] = $matchId;
-            }
-        }
+    //         if (!in_array($matchId, $uniqueMatchIds)) {
+    //             $uniqueMatchIds[] = $matchId;
+    //         }
+    //     }
 
-        // Count the unique match IDs
-        $userMatchCount = count($uniqueMatchIds);
+    //     // Count the unique match IDs
+    //     $userMatchCount = count($uniqueMatchIds);
 
-        return $userMatchCount;
-    }
+    //     return $userMatchCount;
+    // }
 }
