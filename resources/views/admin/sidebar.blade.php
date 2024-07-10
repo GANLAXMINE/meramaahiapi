2af646a84708d6192895c295063587416370ff8d
GANLAXMINE <ganlaxmineprivatelimited@gmail.com>
2024-07-10 23:03:24 +0530

    changes

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,10 +1,10 @@
 <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 ps bg-white" id="sidenav-main">
   <div class="sidenav-header" style="height:5.875rem !important">
     <i class="fas fa-times p-3 cursor-pointer text-dark opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-    <a class="navbar-brand m-0" href="{{ url('/admin/dashboard') }}">
+    <!-- <a class="navbar-brand m-0" href="{{ url('/admin/dashboard') }}">
       <img style="max-height: 10rem !important;width:300px" src="{{ url('img/admin_img.png') }}" class="navbar-brand-img h-100" alt="main_logo">
       {{-- <span class="ms-1 font-weight-bold text-dark">DJ That</span> --}}
-    </a>
+    </a> -->
   </div>
   <hr class="horizontal light mt-0 mb-2">
   {{-- <div class="collapse navbar-collapse  w-auto  max-height-vh-100" id="sidenav-collapse-main"> --}}
@@ -26,14 +26,14 @@
           <span class="nav-link-text ms-1">Users</span>
         </a>
       </li>
-      <li class="nav-item">
+      <!-- <li class="nav-item">
         <a class="nav-link text-dark " href="{{ url('/admin/notification/form') }}">
           <div class="text-dark text-center me-2 d-flex align-items-center justify-content-center">
             <i class="fa-solid fa-bell"></i>
           </div>
           <span class="nav-link-text ms-1">Push Notifications</span>
         </a>
-      </li>
+      </li> -->
       <li class="nav-item">
         <a class="nav-link text-dark " href="{{ url('/admin/match') }}">
           <div class="text-dark text-center me-2 d-flex align-items-center justify-content-center ">
