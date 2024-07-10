ecbb5ff721e3be78ebb99c6ad4010ec24f366a12
GANLAXMINE <ganlaxmineprivatelimited@gmail.com>
2024-07-10 21:24:55 +0530

    changes

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -85,27 +85,7 @@
                 </div>
               </a>
             </div>
-            <!-- <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-              <a href="{{ url('/admin/match') }}">
-                <div class="card">
-                  <div class="card-header p-3 pt-2">
-                    <div class="icon icon-lg icon-shape bg-gradient-primary shadow-primary text-center border-radius-xl mt-n4 position-absolute">
-                      <i class="material-icons opacity-10">person</i>
-                    </div>
-                    <div class="text-end pt-1">
-                      <p class="text-sm mb-0 text-capitalize font-weight-bolder">Total Matches</p>
-                      <h4 class="mb-0">{{$userCount}}</h4>
-                    </div>
-                  </div>
-                  <hr class="dark horizontal my-0">
-                  <div class="card-footer p-3">
-                    {{-- <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+3% </span>than lask month</p> --}}
-                    {{-- <p class="mb-0"><span class="text-success text-sm font-weight-bolder">Total </span>{{ count($user_role) }}</p> --}}
-                  </div>
-                  
-                </div>
-              </a>
-            </div> -->
+          
             <!-- <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
               <a href="#">
                 <div class="card">
